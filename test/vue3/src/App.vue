1e1f409765586d17a51b601bc7e7430646b0e5ba
卷心菜汪 <Rv_Jiang@outlook.com>
2024-01-28 17:41:39 +0800

    chrome: :construction_worker: add the vue3 test environment

---
+++ test/vue3/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const h1 = ref("WelCome To Vue3");
+</script>
+
+<template>
+  <h1>
+    {{ h1 }}
+  </h1>
+</template>
+
+<style scoped>
+div {
+  color: red;
+}
+</style>
