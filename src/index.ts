d4b53ea645855eb8884b97bd678efcdb2cda1452
卷心菜汪 <Rv_Jiang@outlook.com>
2024-01-29 16:08:40 +0800

    :label:chore: expose internal common types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+export type * from "./types/Operations";
+export type * from "./types/Origin";
+export type * from "./types/Report";
+export type * from "./types/Rule";
+
 import { calc } from "@/calculator";
 
 export { calc };
